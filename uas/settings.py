988dabbcccad1e4c833dd1e9d00a57110e8c9296
Andika Pangestu <85173179+andika262@users.noreply.github.com>
2023-01-05 22:31:07 +0700

    Update settings.py

--- uas/settings.py
+++ uas/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import mimetypes
+
 mimetypes.add_type("text/css", ".css", True)
 
 
@@ -125,10 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 
